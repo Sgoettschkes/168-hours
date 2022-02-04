cdbc5400b62372cba767cfe6cdb009861c6f4747
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2022-02-04 17:25:00 +0100

    implement uploading of csv and rendering of the table with the content

--- lib/onehundredsixtyeight_hours_web/live/page/index_live.ex
+++ lib/onehundredsixtyeight_hours_web/live/page/index_live.ex
@@ -5,9 +5,29 @@ defmodule OnehundredsixtyeightHoursWeb.Page.IndexLive do
     OnehundredsixtyeightHoursWeb.PageView.render("index.html", assigns)
   end
 
-  def mount(_params, _, socket) do
-    data = OnehundredsixtyeightHours.Csv.run()
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:data, [])
+     |> allow_upload(:timesheet, accept: ~w(.csv))}
+  end
 
-    {:ok, assign(socket, %{data: data})}
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("save", _params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :timesheet, fn %{path: path}, _entry ->
+        {:ok,
+         File.stream!(path)
+         |> OnehundredsixtyeightHours.Csv.parse()}
+      end)
+
+    data = List.first(uploaded_files)
+
+    {:noreply,
+     socket
+     |> update(:data, fn _ -> data end)}
   end
 end
